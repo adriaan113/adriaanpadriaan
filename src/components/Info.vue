998d3ca8b39482ec2b96cf2ba1407e8e0b93b4e5
Adriaan van der Ploeg <adriaan@MacBook-Air-van-Adriaan.local>
2021-01-10 17:41:37 +0100

    added floating head animation

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,184 +1,280 @@
 <template>
-    <div class="info" @mousemove="eyeMove">
-        <div class="about-me">
-            <h1>Adriaan van der Ploeg...</h1>
-            <p>is fotograaf en beeldredacteur bij de Volkskrant</p>
-            <p>maar ook enthousiast tekenaar van hoofden</p>
-            <p>en front-end enthousiasteling</p>
-           
-            <div class="portrait">
+  <div class="info" @mousemove="eyeMove" @click="floatingHead">
+    <div class="about-me">
+      <h1>Adriaan van der Ploeg...</h1>
+      <p>is fotograaf en beeldredacteur bij de Volkskrant</p>
+      <p>maar ook enthousiast tekenaar van hoofden</p>
+      <p>en front-end enthousiasteling</p>
 
-                <img :src="portretNew" alt="portret van adriaan" class="face">
+    <div class="portrait">
+        <img :src="portretNew" alt="portret van adriaan" class="face" />
 
-                <div class="eyes">
-                    <img :src="eyeLeft" alt="" class="eye left">
-                    <img :src="eyeRight" alt="" class="eye right">
-                </div>
+        <div class="eyes">
+          <img :src="eyeLeft" alt="" class="eye left" />
+          <img :src="eyeRight" alt="" class="eye right" />
+        </div>
 
-                <svg viewBox="0 0 37 0" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="satan">
-                    <defs>
-                        <filter x="-31.1%" y="-23.5%" width="162.2%" height="146.9%" filterUnits="objectBoundingBox" id="b">
-                        <feMorphology radius="3" in="SourceAlpha" result="shadowSpreadInner1"/>
-                        <feGaussianBlur stdDeviation="6" in="shadowSpreadInner1" result="shadowBlurInner1"/>
-                        <feOffset dy="8" in="shadowBlurInner1" result="shadowOffsetInner1"/>
-                        <feComposite in="shadowOffsetInner1" in2="SourceAlpha" operator="arithmetic" k2="-1" k3="1" result="shadowInnerInner1"/>
-                        <feColorMatrix values="0 0 0 0 0.506085247 0 0 0 0 0.066815074 0 0 0 0 0.301744044 0 0 0 0.5 0" in="shadowInnerInner1"/>
-                        </filter>
-                        <filter x="-124.6%" y="-4.6%" width="349.1%" height="109.2%" filterUnits="objectBoundingBox" id="c">
-                        <feGaussianBlur stdDeviation=".6" in="SourceGraphic"/>
-                        </filter>
-                        <path d="M.787 0h35.426c.512 3.482.787 7.175.787 11 0 20.987-8.283 38-18.5 38S0 31.987 0 11C0 7.175.275 3.482.787 0z" id="a"/>
-                    </defs>
-                    <g fill="none" fill-rule="evenodd">
-                        <use fill="#EA0000" xlink:href="#a"/>
-                        <use fill="#000" filter="url(#b)" xlink:href="#a"/>
-                        <path d="M19 9.967C20.929 22.666 20.929 35.919 19 49" fill="#A00C0C" filter="url(#c)"/>
-                    </g>
-                </svg>
+        <!-- <div class="blush-container">
 
-            </div>
-                
-                
-            
-               
-             
+            <svg viewBox="0 0 296 296" xmlns="http://www.w3.org/2000/svg" class="blush-left">
+                <defs>
+                    <filter x="-63.7%" y="-63.7%" width="227.4%" height="227.4%" filterUnits="objectBoundingBox" id="a">
+                    <feGaussianBlur stdDeviation="31" in="SourceGraphic"/>
+                    </filter>
+                </defs>
+                <circle fill="#E90404" filter="url(#a)" cx="148" cy="148" r="73" fill-rule="evenodd"/>
+            </svg>
+
+            <svg viewBox="0 0 296 296" xmlns="http://www.w3.org/2000/svg" class="blush-right">
+                <defs>
+                    <filter x="-63.7%" y="-63.7%" width="227.4%" height="227.4%" filterUnits="objectBoundingBox" id="a">
+                    <feGaussianBlur stdDeviation="31" in="SourceGraphic"/>
+                    </filter>
+                </defs>
+                <circle fill="#E90404" filter="url(#a)" cx="148" cy="148" r="73" fill-rule="evenodd"/>
+            </svg>
+
+        </div> -->
         
-        </div>
 
-        <div class="contact">
-            <h1>contact</h1>
-            <div class="contact-details">
-                <div>
-                    <a href="mailto:adriaanvanderploeg@gmail.com"><p>adriaanvanderploeg@gmail.com</p></a>
-                    <a href="tel: 0620966933"><p>0620966933</p></a>
-                    <a href="https://www.linkedin.com/in/adriaanvanderploeg" target="_blank"><p>LinkedIn</p></a>
-                </div>
-                <div>
-                </div>
-            </div>
-        </div>
+        <svg
+          viewBox="0 0 37 0"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          id="tong"
+        >
+          <defs>
+            <filter
+              x="-31.1%"
+              y="-23.5%"
+              width="162.2%"
+              height="146.9%"
+              filterUnits="objectBoundingBox"
+              id="b"
+            >
+              <feMorphology
+                radius="3"
+                in="SourceAlpha"
+                result="shadowSpreadInner1"
+              />
+              <feGaussianBlur
+                stdDeviation="6"
+                in="shadowSpreadInner1"
+                result="shadowBlurInner1"
+              />
+              <feOffset
+                dy="8"
+                in="shadowBlurInner1"
+                result="shadowOffsetInner1"
+              />
+              <feComposite
+                in="shadowOffsetInner1"
+                in2="SourceAlpha"
+                operator="arithmetic"
+                k2="-1"
+                k3="1"
+                result="shadowInnerInner1"
+              />
+              <feColorMatrix
+                values="0 0 0 0 0.506085247 0 0 0 0 0.066815074 0 0 0 0 0.301744044 0 0 0 0.5 0"
+                in="shadowInnerInner1"
+              />
+            </filter>
+            <filter
+              x="-124.6%"
+              y="-4.6%"
+              width="349.1%"
+              height="109.2%"
+              filterUnits="objectBoundingBox"
+              id="c"
+            >
+              <feGaussianBlur stdDeviation=".6" in="SourceGraphic" />
+            </filter>
+            <path
+              d="M.787 0h35.426c.512 3.482.787 7.175.787 11 0 20.987-8.283 38-18.5 38S0 31.987 0 11C0 7.175.275 3.482.787 0z"
+              id="a"
+            />
+          </defs>
+          <g fill="none" fill-rule="evenodd">
+            <use fill="#EA0000" xlink:href="#a" />
+            <use fill="#000" filter="url(#b)" xlink:href="#a" />
+            <path
+              d="M19 9.967C20.929 22.666 20.929 35.919 19 49"
+              fill="#A00C0C"
+              filter="url(#c)"
+            />
+          </g>
+        </svg>
+
+      </div>
+       <div class="face-shadow"></div>
     </div>
+
+    <div class="contact">
+      <h1>contact</h1>
+      <div class="contact-details">
+        <div>
+          <a href="mailto:adriaanvanderploeg@gmail.com"
+            ><p>adriaanvanderploeg@gmail.com</p></a
+          >
+          <a href="tel: 0620966933"><p>0620966933</p></a>
+          <a
+            href="https://www.linkedin.com/in/adriaanvanderploeg"
+            target="_blank"
+            ><p>LinkedIn</p></a
+          >
+        </div>
+        <div></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 import { gsap } from "gsap";
 
 export default {
-    data(){
-        return{
-            portret: require('../assets/adriaan-van-der-ploeg.jpg'),
-            portretNew: require('../assets/adriaan-van-der-ploeg2.jpg'),
-            eyeLeft: require('../assets/eye-left2.png'),
-            eyeRight: require('../assets/eye-right2.png'),
-            tongueInOut: false
-        }
+  data() {
+    return {
+      portret: require("../assets/adriaan-van-der-ploeg.jpg"),
+      portretNew: require("../assets/adriaan-van-der-ploeg4.png"),
+      eyeLeft: require("../assets/eye-left2.png"),
+      eyeRight: require("../assets/eye-right2.png"),
+      tongueInOut: false,
+    };
+  },
+  methods: {
+    eyeMove: function(e) {
+      const eyes = document.querySelectorAll(".left, .right");
+
+      for (let i = 0; i < eyes.length; i++) {
+        const x =
+          eyes[i].getBoundingClientRect().left + eyes[i].clientWidth / 2;
+        const y =
+          eyes[i].getBoundingClientRect().top + eyes[i].clientHeight / 2;
+        const radian = Math.atan2(e.pageX - x, e.pageY - y);
+        const rot = radian * (180 / Math.PI) * -1 + 90; //was +90
+        eyes[i].style.transform = `rotate(${rot}deg)`;
+      }
     },
-    methods:{
-        eyeMove: function(e){
 
-            const eyes = document.querySelectorAll('.left, .right');  
+    tongue: function() {
+      const tongue = document.querySelector("#tong");
 
-            for(let i=0;i<eyes.length;i++){
-                const x = eyes[i].getBoundingClientRect().left + (eyes[i].clientWidth / 2);
-                const y = eyes[i].getBoundingClientRect().top + (eyes[i].clientHeight / 2);
-                const radian = Math.atan2(e.pageX - x, e.pageY - y);
-                const rot = (radian * (180 / Math.PI) * -1) + 90; //was +90 
-                eyes[i].style.transform = `rotate(${rot}deg)`;
-            }
-        },
-
-        tongue: function (){
-
-               
-
-                const tongue = document.querySelector('#satan');
-
-                
-
-                setInterval(()=>{
-
-
-                if(!this.tongueInOut){
-                    gsap.to(tongue, {attr: {viewBox: "0 0 37 49"},duration: 2,delay:4,});
-                    this.tongueInOut = true;
-                }else if(this.tongueInOut){
-                    gsap.to(tongue, {attr: {viewBox: "0 0 37 0"},duration: 0.5,});
-                    this.tongueInOut= false;
-                }
-
-
-
-                },6000)
-                
-                
-                
-
-               
-
-              
+      setInterval(() => {
+        if (!this.tongueInOut) {
+          gsap.to(tongue, {
+            attr: { viewBox: "0 0 37 49" },
+            duration: 2,
+            delay: 4,
+            ease: "bounce"
+          });
+          this.tongueInOut = true;
+        } else if (this.tongueInOut) {
+          gsap.to(tongue, { attr: { viewBox: "0 0 37 0" }, duration: 0.5 });
+          this.tongueInOut = false;
         }
+      }, 6000);
     },
-    mounted(){
-        this.tongue();
+    // makeMeBlush: function(){
+
+    //     const cheeks= document.querySelector('.blush-container');
+
+    //     const tl = gsap.timeline({repeat: -1, repeatDelay: 5, delay: 10});
+
+    //     cheeks.style.opacity = 0;
+
+    //     tl.to(cheeks, {duration: 3, opacity: 1, delay: 10});
+    //     tl.to(cheeks, {duration: 3, opacity: 0, delay: 5});
+    // },
+    floatingHead: function(){
+      const head= document.querySelector('.portrait');
+      const shadow=document.querySelector('.face-shadow');
+      const tl = gsap.timeline();
+      
+
+      tl.to(head,{y:"random(-10, 10)",duration:1,repeat:-1,yoyo:true, rotation:"random(-5, 5)",repeatRefresh:true,});
+      tl.to(shadow,{scale:1.1,duration:1,repeat:-1,yoyo:true,repeatRefresh:true});
+
     }
-    
-}
+  },
+  mounted() {
+    this.tongue();
+    //this.makeMeBlush();
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+.info {
+  background-color: white;
 
-.info{
-    background-color: wheat;
+  .about-me {
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: flex-start;
+    align-items: center;
 
-    .about-me{
-        display: flex;
-        flex-flow: column nowrap;
-        justify-content: flex-start;
-        align-items: center;
-        // position: relative;
-
-        .portrait{
-            position: relative;
-        }
-
-        .face{
-            width: 80%;
-            //position: relative;
-        }
-
-        .eyes{
-            position: absolute;
-            top: 43%;
-            left: 9%;
-            width: 80%;
-        }
-       
-            
-                .eye{
-                position: absolute;
-                 width: 6%;
-                }
-                .left{
-                   
-                left: 40%;
-                }
-                .right{
-                left: 65%;
-                }
-        }   
-
-        svg{
-            position: absolute;
-            top: 66%;
-            left: 48%;
-            width: 10%;
-        }
-            
-           
-        
-        
-        
+    .portrait {
+      position: relative;
+      margin-bottom: 6rem;
     }
 
+    .face {
+      width: 80%;
+    }
+
+    .eyes {
+      position: absolute;
+      top: 51%;
+      left: -8%;
+      width: 100%;
+    }
+
+    .eye {
+      position: absolute;
+      width: 6%;
+    }
+    .left {
+      left: 39%;
+    }
+    .right {
+      left: 66%;
+    }
+  }
+
+  #tong {
+    position: absolute;
+    top: 81%;
+    left: 45%;
+    width: 9%;
+  }
+
+.blush-container{
+    position: absolute;
+    top: 50%;
+    left: 36%;
+    width: 33%;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    .blush-left,
+    .blush-right{
+        width: 30%;
+        opacity: .2;
+    }
+}
+
+.face-shadow{
+  width:80px;
+  height: 30px;
+  background-color: lighten(lightgray,5%);
+  z-index: 999;
+  margin-top: -80px;
+  border-radius: 40%;
+  filter: blur(6px);
+}
+
+
+}
 </style>
