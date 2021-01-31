d5909f122d46ca484ecf412c7541e6238a6a135f
Adriaan van der Ploeg <adriaan@MacBook-Air-van-Adriaan.local>
2021-01-31 19:50:20 +0100

    added external data file

---
+++ src/components/data.js
@@ -0,0 +1,95 @@
+const work = { 
+    data:[
+        {
+            name: 'Ronnie Flex',
+            job: 'rapper',
+            story: 'Mr. flex at home in Rotterdam. As you can see there’s a freshly made plate of pasta on the table. After the pictures he offered me some. Good sauce!',
+            thumb: require('../assets/thumbs/ronnie-flex-thumb.jpg'),
+            extraImg: [require('../assets/work/ronnie-flex1.jpg'),require('../assets/work/ronnie-flex2.jpg')],
+            hover: false,
+            showMore: false
+        },
+        {
+            name: 'Claudia Cardinale',
+            job: 'moviestar',
+            story: 'is an true diva. In the best sense of the word.',
+            thumb: require('../assets/thumbs/claudia-cardinale-thumb.jpg'),
+            extraImg: [require('../assets/work/claudia-cardinale1.jpg'),require('../assets/work/claudia-cardinale2.jpg')],
+            hover: false,
+            showMore: false
+        },
+        {
+            name: 'Theo Heuft',
+            job: 'in these pictures...',
+            story: "Here you see Theo Heuft. Theo was the owner of a an establishment called Yab Yum. A well known brothel in Amsterdam. Theo has a thousand stories from that time. He told them all when i visited him in France for the Volkskrant. His wife started a Bed&Breakfast there. He's bored out of his mind.",
+            thumb: require('../assets/thumbs/theo-heuft-thumb.jpg'),
+            extraImg: [require('../assets/work/theo-heuft1.jpg'),require('../assets/work/theo-heuft2.jpg'),require('../assets/work/theo-heuft3.jpg'),require('../assets/work/theo-heuft4.jpg')],
+            hover: false,
+            showMore: false,
+        },
+        {
+            name: 'Maarten Spruyt',
+            job: 'stylist',
+            story: 'favorite color appears to green. Both his house and his wardrobe was full of it. Quite remarkable. Besides that he’s also a really nice and talented guy.',
+            thumb: require('../assets/thumbs/maartenSpruyt-thumb.jpg'),
+            extraImg: [require('../assets/work/maarten-spruyt1.jpg'),require('../assets/work/maarten-spruyt2.jpg')],
+            hover: false,
+            showMore: false
+        },
+        {
+            name: 'Fien Troch',
+            job: 'Director',
+            story: 'in her hotel room during the Rotterdam film festival.',
+            thumb: require('../assets/thumbs/fien-troch-thumb.jpg'),
+            extraImg: [require('../assets/work/fien-troch1.jpg'),require('../assets/work/fien-troch2.jpg')],
+            hover: false,
+            showMore: false
+        },
+        {
+            name: 'John de Wolf',
+            job: 'Soccer legend',
+            story: 'John outside the cafeteria of soccer club Sparta.',
+            thumb: require('../assets/thumbs/john-de-wolf-thumb.jpg'),
+            extraImg: [require('../assets/work/john-de-wolf1.jpg'),require('../assets/work/john-de-wolf2.jpg')],
+            hover: false,
+            showMore: false
+        },
+        {
+            name: 'Lubomyr Melnyk',
+            job: 'Musical mastermind',
+            story: 'Lubomyr Melnyk in Brussels before his concert. He invited me to come and listen. Wikipedia says the following regarding mr. Melnyk\'s music: Melnyk is noted for "continuous music", a piano technique based on extremely rapid notes and complex note-series that create a "tapestry of sound". I\'m not sure if i got the whole tapestry of sound thing but it was beautiful nonetheless.',
+            thumb: require('../assets/thumbs/melnik-thumb.jpg'),
+            extraImg: [require('../assets/work/lubomyr-melnyk1.jpg'),require('../assets/work/lubomyr-melnyk2.jpg')],
+            hover: false,
+            showMore: false
+        },
+        {
+            name: 'Omar',
+            job: 'rapper',
+            story: '...is a guy from Amsterdam, a rapper and a writer. I wanted to meet him at his home to make some pictures but he wanted to get cigarettes instead. And so it happened.',
+            thumb: require('../assets/thumbs/omar-thumb.jpg'),
+            extraImg: [require('../assets/work/omar1.jpg'),require('../assets/work/omar2.jpg'),require('../assets/work/omar3.jpg')],
+            hover: false,
+            showMore: false
+        },
+        {
+            name: 'Child of Lov',
+            job: 'musician',
+            story: '...was a good musician and a really nice guy. Unfortunately he died. I made these pictures in Amsterdam.',
+            thumb: require('../assets/thumbs/child-of-love-thumb.jpg'),
+            extraImg: [require('../assets/work/child-of-love1.jpg'),require('../assets/work/child-of-love2.jpg'),require('../assets/work/child-of-love3.jpg')],
+            hover: false,
+            showMore: false
+        },
+        {
+            name: 'Marc Bijl',
+            job: 'Artist',
+            story: 'These pictures were made while he was preparing his show in the \'Groninger museum\'.',
+            thumb: require('../assets/thumbs/marc-bijl-thumb.jpg'),
+            extraImg: [require('../assets/work/marc-bijl1.jpg'),require('../assets/work/marc-bijl2.jpg')],
+            hover: false,
+            showMore: false
+        } 
+    ]
+};
+export default work;
