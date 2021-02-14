d5af6168b870589bc0bf0d34b1a81aa43e207828
Adriaan van der Ploeg <adriaan@MacBook-Air-van-Adriaan.local>
2021-02-14 23:42:10 +0100

    fixed header mistake

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -153,10 +153,8 @@ export default {
     window.document.onscroll = () => {
       let navBar = document.getElementById("nav");
       if (window.scrollY > navBar.offsetTop + 60) {
-        // navBar.style.top = '28px';
         this.active = true;
       } else {
-        // navBar.style.top = '0';
         this.active = false;
       }
     };
@@ -208,13 +206,16 @@ export default {
   @media (min-width: $breakpoint-medium) {
       margin: -30px auto 0;
     }
+  h1,
+  h2 {
+    @media (min-width: $breakpoint-large) {
+      font-size: 8rem;
+    }
+  }
 }
 h1,
 h2 {
   margin: 0.5rem 0;
-  @media (min-width: $breakpoint-large) {
-      font-size: 8rem;
-    }
 }
 h3 {
   margin: 2rem;
@@ -249,7 +250,6 @@ h3 {
   height: 60px;
   z-index: 999;
   top: 0;
-  // padding: 1rem 0;
   // margin-top: 1rem; // ZOU DIT WERKEN???
   @media screen and (min-width: $breakpoint-medium) {
     flex-direction: row;
